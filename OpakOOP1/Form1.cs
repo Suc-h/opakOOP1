bec0560dad4191c672875b0c5b0ae2587d643d15
Suc-h <97514706+Suc-h@users.noreply.github.com>
2023-09-13 11:24:47 +0200

    FTSIO
    tadaaa
    ftsio

---
+++ OpakOOP1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpakOOP1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zamestnanec ondra = new Zamestnanec("Miloš", "Ondřej Zelený",200000,dateTimePicker1.Value);
+            MessageBox.Show(ondra.ToString());
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OpakOOP1/Zamestnanec.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace OpakOOP1
+{
+    class Zamestnanec
+    {
+        private string titul;
+        private string jmeno;
+        private string pozice;
+        private int mplat;
+        private DateTime dndp;
+        private int bonus;
+        private int hodiny;
+        private int relaxaccka;
+
+        public Zamestnanec(string titul, string jmeno, int mplat, DateTime dndp)
+        {
+            this.Titul = titul;
+            this.Jmeno = jmeno;
+            this.mplat = mplat;
+            this.dndp = dndp;
+            this.bonus = 0;
+            this.hodiny = 0;
+        }
+
+        string Jmeno
+        {
+            get
+            {
+                return jmeno;
+            }
+            set
+            {
+
+                string [] a = jmeno.Split(' ');
+                string jmenoo = a[1];
+                string prijmeni = a[2];
+                string b = jmenoo.First().ToString();
+                jmenoo.Replace(jmenoo.First(), Convert.ToChar(b.ToUpper()));
+                prijmeni.ToUpper();
+                jmeno = jmenoo + prijmeni;
+
+            }
+
+        }
+        string Titul
+        {
+            get
+            {
+                return titul;
+            }
+            set
+            {
+                titul.Trim();
+                string a = titul.First().ToString();
+                titul.Replace(titul.First(), Convert.ToChar(a.ToUpper()));
+                titul.Insert(titul.Length, ".");
+
+            }
+        }
+        public int rocnipPlat(int plat)
+        {
+            mplat=plat;
+            return 12 * mplat;
+        }
+        public int pocetMesicu()
+        {
+            TimeSpan dnes = DateTime.Now - dndp;
+            return (dnes.Days / 30);
+        }
+        public int celkem()
+        {
+            int castka = pocetMesicu() * mplat;
+            return castka;
+
+        }
+        public void zvysOsobni(int castka)
+        {
+            bonus = bonus + castka;
+            if(bonus>10000)
+            {
+                MessageBox.Show("Zaměstnanec je vedoucím!");
+                pozice = "Vedoucí";
+            }
+        }
+        public void odpracHod(int hodina)
+        {
+            hodiny = hodiny + hodina;
+            if(hodiny>=100)
+            {
+                MessageBox.Show("Musíš relaxovat!");
+            }
+        }
+        public void relaxace(int relaxaceH)
+        {
+            relaxaccka = relaxaccka + relaxaceH;
+            if(relaxaccka>=72)
+            {
+                relaxaccka = 0;
+                hodiny = 0;
+            }
+        }
+        public override string ToString()
+        {
+            return titul + " " + jmeno + " " + "Plat:" + mplat + " " + celkem() + " " + bonus + " " + rocnipPlat(mplat) + " " + hodiny;
+        }
+    }
+}
